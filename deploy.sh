ed393b6779e764e7a9acf9e3b90aa78fe53f6c8e
David Jopson <duggum@users.noreply.github.com>
2015-10-27 12:33:47 -0400

    Add debug statements to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,16 @@
 set -e # exit with nonzero exit code if anything fails
 
 # clear and re-create the out directory
+echo "Removing jsdoc . . ."
 rm -rf jsdoc || exit 0;
 # mkdir out;
 
 # run our compile script, discussed above
+echo "Running grunt . . ."
 ./grunt
 
 # go to the out directory and create a *new* Git repo
+echo "Initialize repo . . ."
 cd jsdoc
 git init
 
@@ -18,6 +21,7 @@ git config user.email "duggum@users.noreply.github.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
+echo "Commit changes . . ."
 git add .
 git commit -m "Deploy to GitHub Pages"
 
@@ -25,4 +29,5 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+echo "Push to gh-pages . . ."
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
