48ff35ecc0d240b03cc77e57069b0a3bee708d27
David Jopson <duggum@users.noreply.github.com>
2015-10-27 14:19:07 -0400

    Add master branch links to gh-pages README

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,8 @@ git init
 echo "Creating README file . . ."
 cat > README.md <<EOF
 ## ModPE API Documentation
+#### This branch is automatically built by [Travis CI](https://travis-ci.org/)
+#### For full source code, go to the [master branch](https://github.com/duggum/modpe-api/tree/master)
 [![Build Status](https://travis-ci.org/duggum/modpe-api.svg?branch=master)](https://travis-ci.org/duggum/modpe-api)
 EOF
 
